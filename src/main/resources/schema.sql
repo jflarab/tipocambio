aaacbc8bba1eabda2aef1ec735b7a45b77863887
unknown <junior.lara@rimac.com.pe>
2023-05-24 17:46:08 -0500

    jwt authentication

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
 CREATE TABLE tipocambio (id INT PRIMARY KEY,
 						 monedaorigen VARCHAR(25),
 						 monedadestino VARCHAR(25),
-						 tipocambio DECFLOAT(20));
+						 tipocambio DECIMAL(20,2));
--- src/test/java/com/desafio/tipocambio/TipocambioApplicationTests.java
+++ src/test/java/com/desafio/tipocambio/TipocambioApplicationTests.java
@@ -1,13 +1,10 @@
 package com.desafio.tipocambio;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TipocambioApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
